a2948b55243ca152fd69c3c53eb6a07c8362dda2
Hiroyuki Kuga <negitorox149@gmail.com>
2020-09-28 16:50:23 +0900

    isDone 実装

--- js/main.js
+++ js/main.js
@@ -1,12 +1,38 @@
-(function() {
-  'use strict';
+(function () {
+  "use strict";
 
   // two way data binding (to UI)
 
-  var vm = new Vue ({
+  var vm = new Vue({
     el: "#app",
     data: {
-      name: 'taguchi'
+      newItem: "",
+      todos: [
+        // {
+        //   title: "task 1",
+        //   isDone: false
+        // },
+        // {
+        //   title: "task 2",
+        //   isDone: false
+        // },
+        // {
+        //   title: "task 3",
+        //   isDone: true
+        // }
+      ]
+    },
+    methods: {
+      addItem: function (e) {
+        // e.preventDefault();
+        this.todos.push(this.newItem);
+        this.newItem = "";
+      },
+      deleteItem: function (index) {
+        if (confirm("Are you sure?")) {
+          this.todos.splice(index, 1);
+        }
+      }
     }
-  })
+  });
 })();
