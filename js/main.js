698aeb4d182d37a4ff2582591d3e4582106678c7
Hiroyuki Kuga <negitorox149@gmail.com>
2020-09-30 12:25:34 +0900

    localstorage added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   "use strict";
 
   // two way data binding (to UI)
@@ -22,16 +22,48 @@
         // }
       ]
     },
+    computed: {
+      remaining: function() {
+        var items = this.todos.filter(function (todo) {
+          return !todo.isDone;
+        });
+        return items;
+      }
+    },
+    watch: {
+      // todos: function() {
+      //   localStorage.setItem('todos', JSON.stringify(this.todos));
+      //   alert('Data saved.');
+      todos: {
+        handler: function() {
+          localStorage.setItem('todos', JSON.stringify(this.todos));
+        },
+        deep: true
+      }
+    },
+    mounted: function() {
+      this.todos = JSON.parse(localStorage.getItem('todos') || []);
+    },
     methods: {
-      addItem: function (e) {
+      addItem: function(e) {
         // e.preventDefault();
-        this.todos.push(this.newItem);
+        var item = {
+          title: this.newItem,
+          isDone: false
+        };
+        this.todos.push(item);
         this.newItem = "";
       },
-      deleteItem: function (index) {
+      deleteItem: function(index) {
         if (confirm("Are you sure?")) {
           this.todos.splice(index, 1);
         }
+      },
+      purge: function (index) {
+        if (!confirm("delete finished?")) {
+          return;
+        }
+        this.todos = this.remaining;
       }
     }
   });
