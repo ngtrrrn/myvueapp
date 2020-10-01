0551cffd2456bcc2ba4134e30b0ef24df3566cd4
Hiroyuki Kuga <negitorox149@gmail.com>
2020-10-01 09:43:32 +0900

    template, incrementTotal

--- js/main.js
+++ js/main.js
@@ -1,70 +1,39 @@
 (function() {
   "use strict";
 
-  // two way data binding (to UI)
-
-  var vm = new Vue({
-    el: "#app",
-    data: {
-      newItem: "",
-      todos: [
-        // {
-        //   title: "task 1",
-        //   isDone: false
-        // },
-        // {
-        //   title: "task 2",
-        //   isDone: false
-        // },
-        // {
-        //   title: "task 3",
-        //   isDone: true
-        // }
-      ]
-    },
-    computed: {
-      remaining: function() {
-        var items = this.todos.filter(function (todo) {
-          return !todo.isDone;
-        });
-        return items;
+  var likeComponent = Vue.extend({
+    props: {
+      message: {
+        type: String,
+        default: 'default'
       }
     },
-    watch: {
-      // todos: function() {
-      //   localStorage.setItem('todos', JSON.stringify(this.todos));
-      //   alert('Data saved.');
-      todos: {
-        handler: function() {
-          localStorage.setItem('todos', JSON.stringify(this.todos));
-        },
-        deep: true
+    data: function() {
+      return {
+        count: 0
       }
     },
-    mounted: function() {
-      this.todos = JSON.parse(localStorage.getItem('todos') || []);
-    },
+    template: '<button @click="countUp">{{ message }} {{ count }}</button>',
     methods: {
-      addItem: function(e) {
-        // e.preventDefault();
-        var item = {
-          title: this.newItem,
-          isDone: false
-        };
-        this.todos.push(item);
-        this.newItem = "";
-      },
-      deleteItem: function(index) {
-        if (confirm("Are you sure?")) {
-          this.todos.splice(index, 1);
-        }
-      },
-      purge: function (index) {
-        if (!confirm("delete finished?")) {
-          return;
-        }
-        this.todos = this.remaining;
+      countUp: function() {
+        this.count++;
+        this.$emit('increment');
       }
     }
   });
+
+  var vm = new Vue({
+    el: "#app",
+    components: {
+      'like-component': likeComponent
+    },
+    data: {
+      total: 0
+    },
+    methods: {
+      incrementTotal: function() {
+        this.total++;
+      }
+    }
+  })
 })();
